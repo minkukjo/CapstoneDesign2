e241bab5edebfb11a2b461f9022a0dcd232f3afc
Ubuntu <ubuntu@ip-172-31-16-14.ap-northeast-2.compute.internal>
2019-04-05 04:49:10 +0000

    Fix server response

--- VoiceService/server/server.py
+++ VoiceService/server/server.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['m4a','txt'])
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['m4a','txt','wav'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,10 +16,13 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        file.save(secure_filename(file.filename))
+
+		#UPLOAD_FOLDER is not a configuration option recognized by Flask
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
         print ('save success')
+	return 'OK'
 
-app.run(debug=True)
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=5000,debug=True)
+
